2aca9bac22370dd31e08cae95b9dcc082570780c
shelotriadpoberegy <116545879+shelotriadpoberegy@users.noreply.github.com>
2023-01-08 16:52:59 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = "C:\chromedriver\chromedriver.exe"
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'dogmarta1@gmail.com'
+    valid_password = 'martadog'
+    valid_email_for_reg = 'dogmarta1@mail.ru'
+    invalid_email = 'dogmarta@gmail.com'
+    invalid_password = 'Qw12345678Wq543'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Иван'
+    l_name = 'Иванов'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'и'
+    latin_generate_string = 'ssttww'
+    chinese_chars = '我不会说汉语请做不辣的怎么样你会说英语'
+    special_chars = '!@#$$%^&*()_+_+=//{}'
+    numbers = 987
+    empty = ''
+    passw1 = 'qQ654321'
+    passw2 = 'Qw12345678Wq543'
+    passw3 = 'Qa12345678Er12345978'
